74ce384b7a4a1d93c2b55cae28305b7b1b781212
Orie Chinedu <oriechinedu@gmail.com>
2019-05-15 14:34:58 +0100

    Add Greensock Animation to article container

--- Article/Article.js
+++ Article/Article.js
@@ -14,12 +14,14 @@ class Article {
 
   expandArticle = () => {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open')
+  
     if (this.expandButton.textContent === 'Expand') {
       this.expandButton.textContent = 'Show Less'
     } else {
       this.expandButton.textContent = 'Expand'
     }
+    TweenMax.from(this.domElement, 0.5, {x:300, y: -200, opacity:0, scale:0.5});
+    this.domElement.classList.toggle('article-open')
   }
 }
 
